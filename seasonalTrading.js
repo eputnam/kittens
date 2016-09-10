02ec4f2b960091e2d89ad2e2032c04e483857d93
Eric Putnam <putnam.eric@gmail.com>
2016-09-09 20:14:05 -0700

    fixes redundant appearances

--- seasonalTrading.js
+++ seasonalTrading.js
@@ -9,55 +9,43 @@ javascript: (function() {
     var raceElements = document.getElementsByClassName('panelContainer');
     var season = document.getElementById('calendarDiv').textContent;
 
+    var colorHighest = "#00cc00";
+    var colorHigh = "#009900";
+    var colorAverage = "#006600";
+    var colorLow = "#003300";
+    var colorLowest = "#001a00";
 
     function statusColor(status){
         var colorHex = "";
         switch(status.toLowerCase()){
         case "highest":
-            colorHex = "#1f7a25";
+            colorHex = colorHighest;
             break;
         case "high":
-            colorHex = "#349f3b";
+            colorHex = colorHigh;
             break;
         case "average":
-            colorHex = "#eede51";
+            colorHex = colorAverage;
             break;
         case "low":
-            colorHex = "#4888D8";
+            colorHex = colorLow;
             break;
         case "lowest":
-            colorHex = "#2f5a90";
+            colorHex = colorLowest;
             break;
         }
         return colorHex;
     }
 
-    function statusText(status){
-        var text = "";
-        switch(status.toLowerCase()){
-        case "highest":
-            text = " - Highest";
-            break;
-        case "high":
-            text = " - High";
-            break;
-        case "average":
-            text = " - Average";
-            break;
-        case "low":
-            text = " - Low";
-            break;
-        case "lowest":
-            text = " - Lowest";
-            break;
-        }
-        return text;
-    }
-
     function setAvailability(raceElement, status){
         raceElement.children[2].children[1].children[0].children[0].setAttribute("style", "background-color:" + statusColor(status));
-        raceElement.children[2].children[1].children[0].children[0].innerHTML += statusText(status);
     }
+
+    var statusBar = ' <style> .statusKeyItem {font-family: sans-serif; font-size: 14px; text-shadow: 1px 1px 0 #444; display: inline-block; height: 20px; width: 75px; border: solid #DE8D47 1px; border-radius:3px; text-align:center;} </style> <div id="statusKey" style="height:50px; display:block;"> <div class="statusKeyItem" style="background-color:' + colorHighest + '">Highest</div> <div class="statusKeyItem" style="background-color:' + colorHigh + '">High</div> <div class="statusKeyItem" style="background-color:' + colorAverage + '">Average</div> <div class="statusKeyItem" style="background-color:' + colorLow + '">Low</div> <div class="statusKeyItem" style="background-color:' + colorLowest + '">Lowest</div></div>';
+
+
+    $('<div id="placeholder"></div>').insertBefore('.expandAllBar');
+    document.getElementById("placeholder").innerHTML = statusBar;
 
 
     if(season.includes("Autumn")){
